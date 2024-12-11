3b109cc3c6ccdf1c100e8b8192b4af9f835d8321
FireHeart003 <65786491+FireHeart003@users.noreply.github.com>
2024-12-11 13:21:29 -0500

    Added Comments to Trash Removal Web Service to explain the logic and implementation

--- Trash Removal/script.js
+++ Trash Removal/script.js
@@ -4,7 +4,8 @@ const readEP = cascadeEP + "/api/v1/read";
 const createEP = cascadeEP + "/api/v1/create";
 const APIKey = config.API_KEY;
 
-async function getSites() {
+// Updates site to have a recycleBinExpiration of 30 days
+async function trash() {
     document.getElementById("output").textContent = "";
     try {
         var response = await fetch( cascadeEP + "/api/v1/listSites" , {
@@ -23,17 +24,19 @@ async function getSites() {
         
         //loop through the data in the json variable and output the name of the site 
         json.sites.forEach(site => {
-            let siteOptions = readAsset("site",site.id);
+            let siteOptions = readAsset("site",site.id); // Read the site info 
             siteOptions.then((asset) => {
-                if(typeof asset !== 'undefined'){
+                if(typeof asset !== 'undefined'){ // Checks if the asset exists
                     console.log(asset);
                     let check = asset.site.recycleBinExpiration;
                     let name = asset.site.name;
                     let siteId = site.id;
-                    if(check != "30"){
-                        asset.site.recycleBinExpiration = "30";
-                        editAsset("site", siteId, asset).then((data) =>{
-                            if(data.success === true){
+                    if(check != "30"){ // Checks if the recycleBinExpiration is not set to 30 days
+                        asset.site.recycleBinExpiration = "30"; // Set the recyleBinExpiration to 30 days for the asset
+
+                        // Call the editAsset method to update the asset with a recycleBinExpiration of 30 days
+                        editAsset("site", siteId, asset).then((data) =>{ 
+                            if(data.success === true){ // Outputs the name of the website as a link if asset was successfully edited
                                 document.getElementById("output").innerHTML += `<a href="https://cascade.fiu.edu/site/open.act?action=edit&id=${siteId}" target="_blank" rel="noopener noreferrer">${name}</a><br>`;
                                 console.log("Successfully edited" + name);
                             }else{
@@ -48,8 +51,8 @@ async function getSites() {
         console.error(`: ${error}`);
     }
 }
-//2c17a7a70a73710b3f55154592ef5dd6
 
+// Manually testing a site
 async function getSites2() {
     document.getElementById("output").textContent = "";
     try {
@@ -68,13 +71,13 @@ async function getSites2() {
         var json = await response.json();
         
         //loop through the data in the json variable and output the name of the site 
-            let siteOptions = readAsset("site","2c17a7a70a73710b3f55154592ef5dd6");
+            let siteOptions = readAsset("site","Insert site id here");
             siteOptions.then((asset) => {
                 if(typeof asset !== 'undefined'){
                     console.log(asset);
                     let check = asset.site.recycleBinExpiration;
                     let name = asset.site.name;
-                    let siteId = "2c17a7a70a73710b3f55154592ef5dd6";
+                    let siteId = "insert site id here";
                     if(check != "30"){
                         asset.site.recycleBinExpiration = "30";
                         editAsset("site", siteId, asset).then((data) =>{
@@ -92,6 +95,8 @@ async function getSites2() {
         console.error(`: ${error}`);
     }
 }
+
+// Reads an asset given an id and type and returns a JSON object of the asset
 async function readAsset(type,id) {
     var assetType = type;
     var assetID = id;
@@ -117,6 +122,7 @@ async function readAsset(type,id) {
     }
 }
 
+// Edits an asset in Cascade with the given parameters for the update
 async function editAsset(type, id, asset) {
 
     try {
