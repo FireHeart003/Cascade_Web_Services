3f97ae8cd9b7b30919b971ec92d78455573683cb
FireHeart003 <65786491+FireHeart003@users.noreply.github.com>
2024-12-17 09:28:37 -0500

    Added Comments to User of Groups Web service to explain the logic and implementation

--- UserOfGroups/script.js
+++ UserOfGroups/script.js
@@ -8,11 +8,12 @@ const readEP = cascadeEP + "/api/v1/read";
 const createEP = cascadeEP + "/api/v1/create";
 const APIKey = config.API_KEY;
 
-async function getSites() {
+async function getGroupUsers() {
     document.getElementById("output").textContent = "";
-    const set = new Set();
+    const set = new Set(); // Store users in a set (no duplicates allowed)
 
     try {
+        // Fetch all the sites given the API Key
         const response = await fetch(cascadeEP + "/api/v1/listSites", {
             headers: {
                 Authorization: "Bearer " + APIKey,
@@ -27,21 +28,22 @@ async function getSites() {
             throw new Error(`HTTP error: ${response.status}`);
         }
 
+        // Convert result to json
         const json = await response.json();
 
         // Process sites sequentially
         for (const site of json.sites) {
-            const asset = await readAsset("site", site.id);
+            const asset = await readAsset("site", site.id); // Read the site info
 
-            if (asset?.site?.roleAssignments?.[0]?.users) {
-                const roles = asset.site.roleAssignments;
-                for (const role of roles) {
-                    if (role.users !== undefined) continue;
+            if (asset?.site?.roleAssignments?.[0]?.users) { // Checks if there are any users associated with the site id
+                const roles = asset.site.roleAssignments; // Gets the array of users
+                for (const role of roles) { // Loop through each role
+                    if (role.users !== undefined) continue; // Ignore normal users
 
-                    if (role.groups !== undefined) {
+                    if (role.groups !== undefined) { // If there are groups
                         console.log(asset)
-                        const nameArr = role.groups.split(",");
-                        for (const item of nameArr) {
+                        const nameArr = role.groups.split(","); // Names are in a string seperated by ','
+                        for (const item of nameArr) { // Loop through each name and add them to the set
                             set.add(item);
                         }
                     }
@@ -49,26 +51,29 @@ async function getSites() {
             }
         }
 
-     // Call the finished function
+     // Call the finished function to output set
      finished(set);
     } catch (error) {
         console.error(`Error: ${error}`);
     }
 }
 
+// Generates the output
 async function finished(set) {
     let htmlText = "";
     const nameSet = new Set();
-    for (const value of set) {
-        const response = await readAsset("group", value)
+    for (const value of set) { // Loop through each group name
+        const response = await readAsset("group", value) // Get info of the users in the group
         console.log(response);
         const names = response.group.users;
         const nameArr = names.split(";");
-        for(const name of nameArr){
+        for(const name of nameArr){ // Loop through each name and add them to the set
             nameSet.add(name);
         }
     }
     console.log(nameSet)
+
+    // Print out the email of the user
     for(const userName of nameSet){
         htmlText += userName + "@fiu.edu, ";
     }
@@ -77,6 +82,7 @@ async function finished(set) {
 }
 
 
+// Reads an asset given an id and type and returns a JSON object of the asset
 async function readAsset(type,id) {
     var assetType = type;
     var assetID = id;
@@ -102,6 +108,7 @@ async function readAsset(type,id) {
     }
 }
 
+// Creates an asset in Cascade
 async function createAsset(asset) {
     try {
         var response = await fetch( createEP, {
@@ -125,10 +132,9 @@ async function createAsset(asset) {
     }
 }
 
+// Edits an asset in Cascade with the given parameters for the update
 async function editAsset(type, id, asset) {
-
     try {
-
         let response = await fetch( editEP + "" + "/"+ type +"/" + id, 
             { 
                 method: 'POST',
